e81a1cfc2b4e1035e6d9d30df82d620e453fda84
mznide <matej.znideric@gmail.com>
2016-08-29 06:36:40 -0700

    bench.sh

--- bench/bench.sh
+++ bench/bench.sh
@@ -10,9 +10,16 @@ logfile="$name.txt"
 
 test -f $logfile && exit
 
-n=1024
-while (( n <= 16384 )); do
-	$EXE --size $n | tee -a $logfile
-	(( n += 1024 ))
+m=0
+
+while (( m <= 5)); do
+	n=1024
+	while (( n <= 16384 )); do
+		sleep 2
+		$EXE --size $n | tee -a $logfile
+		(( n += 1024 ))
+	done
+
+	(( m += 1 ))
 done
 
